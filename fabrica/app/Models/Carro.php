12452bfa764bdeee09f7b538c4ebcfa9b565ef55
Hederblz <matheusmr640@gmail.com>
2022-07-10 10:41:40 -0300

    config Model carro e user

--- fabrica/app/Models/Carro.php
+++ fabrica/app/Models/Carro.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Carro extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'marca',
+        'modelo',
+        'placa',
+        'roda',
+        'user_id'
+    ];
+
+    public function dono(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
--- fabrica/app/Models/User.php
+++ fabrica/app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function carros(){
+        return $this->hasMany(camisetas::class, 'user_id');
+    }
+
 }
