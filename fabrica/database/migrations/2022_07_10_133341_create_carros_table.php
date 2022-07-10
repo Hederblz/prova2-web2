afa4a3defd968f0a581027d9aa10e49165c38bd3
Hederblz <matheusmr640@gmail.com>
2022-07-10 10:38:03 -0300

    Criando model Carro

---
+++ fabrica/database/migrations/2022_07_10_133341_create_carros_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carros', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('marca');
+            $table->string('modelo');
+            $table->string('placa');
+            $table->string('roda');
+            $table->foreignId('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carros');
+    }
+};
